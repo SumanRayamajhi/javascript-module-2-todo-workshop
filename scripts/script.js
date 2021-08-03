39810f6eeb8471a64fd4ec54089ffec68a347446
Suman Rayamajhi <meh_sams@yahoo.com>
2021-08-03 22:53:59 +0200

    completed first week

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,77 @@
-alert("hello!");
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log(text)
+})
+
+let todos = []
+
+const createTodo = (text) => {
+    todos.push(text)
+}
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+
+    if (text.length > 0) {
+        createTodo(text)
+        e.target.elements.text.value = ''
+    }
+
+    renderTodos(todos)
 })
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+    // Setup the todo text
+    todoText.textContent = todo
+    containerEl.appendChild(todoText)
+
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    // Setup the remove button
+    const removeButton = document.createElement('button')
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todoText)
+        renderTodos(todos)
+    })
+
+    return todoEl
+}
+
+const renderTodos = (todos) => {
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    if (todos.length > 0) {
+        todos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
+}
+
+renderTodos(todos);
+
+const removeTodo = (todoEl) => {
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+    })
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
